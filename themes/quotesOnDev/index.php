1b478290e68c12fa120e2e163980362c0b125d0e
Albert Owusu <admin@admins-MacBook-Pro.local>
2020-03-09 18:39:33 -0400

    feat: printed quotes from api

--- themes/quotesOnDev/index.php
+++ themes/quotesOnDev/index.php
@@ -1,7 +1,17 @@
 <?php get_header(); ?>
-
-
+<!-- Random quote loop -->
+<?php if( have_posts() ) : 
+    while( have_posts() ) :
+        the_post(); ?>
+    <!-- Random quote content -->
+    <h2><?php the_title(); ?></h2>
+    <?php the_content(); ?>
+<?php endwhile;?>
+<?php else : ?>
+        <p>No posts found</p>
+<?php endif;?>
+<!-- Random quote generator -->
 <button class="randomQuote">Click Me!</button>
-
-    
+<div class="test-div">
+</div>
 <?php get_footer();?>
--- themes/quotesOnDev/js/api.js
+++ themes/quotesOnDev/js/api.js
@@ -1,5 +1,4 @@
 (function($) {
-  console.log('working', red_vars);
   $('.randomQuote').on('click', function(e) {
     $.ajax({
       method: 'GET',
@@ -7,7 +6,12 @@
         red_vars.rest_url +
         'wp/v2/posts?filter[orderby]rand&filter[posts_per_page]=10'
     }).done(function(data) {
-      console.log(data);
+      $.each(data, function(index, value) {
+        console.log(value, 'value');
+        let test = value.content.rendered;
+        console.log(test);
+        $('.test-div').append(test);
+      });
     }); // Closing done function
   }); // Closing event listener (random quote)
 })(jQuery); // Closing document ready function
