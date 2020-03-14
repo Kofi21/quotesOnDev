97c9c730673234afe2bb6b6386f6f173b6a29e08
Albert Owusu <admin@admins-MacBook-Pro.local>
2020-03-14 18:28:48 -0400

    style: styled footer

--- themes/quotesOnDev/footer.php
+++ themes/quotesOnDev/footer.php
@@ -1,10 +1,10 @@
 <?php wp_footer();?>
 <div class="footer">
     <ul>
-        <li>About</li>
-        <li>Archives</li>
-        <li>Submit a Quote</li>
-        <li>Brought to you by<span>Red Academy</span></li>
+        <li>About <span>&nbsp;&nbsp;&nbsp;|</span></li>
+        <li> Archives <span>&nbsp;&nbsp;&nbsp;|</span></li>
+        <li> Submit a Quote <span>&nbsp;&nbsp;&nbsp;|</span></li>
+        <li> <span> Brought to you by </span>Red Academy</li>
     </ul>
 </div>
 </body>
