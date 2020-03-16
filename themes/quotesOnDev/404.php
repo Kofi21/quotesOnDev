e7026c2790d59e62b918875cbe96c8630adbc546
Albert Owusu <admin@admins-MacBook-Pro.local>
2020-03-16 16:11:11 -0400

    style: styled about page

--- themes/quotesOnDev/404.php
+++ themes/quotesOnDev/404.php
@@ -1,5 +1,17 @@
 <?php get_header(); ?>
+    
+    <div class="error-page">
 
-<h1>You landed on a broken page</h1>
+        <i class="fas fa-quote-left"></i>
+            <div>
+                <h1>Oops</h1>
+                <p>It looks like nothing was found at this location. Maybe try a search?</p>
+                <input placeholder="Search..."><i class="fas fa-search"></i>
+            </div>
+        <i class="fas fa-quote-right"></i>    
+
+    </div>
+    
+
 
 <?php get_footer();?>
