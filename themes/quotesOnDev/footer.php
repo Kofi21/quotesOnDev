9f2ba2b0a89c5eb07ba5a339f9469f13df6758c7
Albert Owusu <admin@admins-MacBook-Pro.local>
2020-03-16 14:01:52 -0400

    feat: added submit page

--- themes/quotesOnDev/footer.php
+++ themes/quotesOnDev/footer.php
@@ -3,7 +3,7 @@
     <ul>
         <li>About <span>&nbsp;&nbsp;&nbsp;|</span></li>
         <li> Archives <span>&nbsp;&nbsp;&nbsp;|</span></li>
-        <li> Submit a Quote <span>&nbsp;&nbsp;&nbsp;|</span></li>
+        <a href="<?php echo get_home_url() . '/submit/';?>"><li> Submit a Quote <span>&nbsp;&nbsp;&nbsp;|</span></li></a>
         <li> <span> Brought to you by </span>Red Academy</li>
     </ul>
 </div>
